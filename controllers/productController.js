ab0ffe14efbfbdbbf6b145f0c98aa83d2202bf0b
Marcus Mathuram <marcus.mathuram7@gmail.com>
2024-09-26 13:41:25 +0530

    Done

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,13 +4,23 @@ const router = express.Router();
 
 // get all products
 router.get('/products', async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
   try {
-    const products = await Product.find({});
-    res.json(products);
+    const products = await Product.find()
+      .skip((page - 1) * limit)
+      .limit(Number(limit));
+    const totalProducts = await Product.countDocuments();
+    res.json({
+      products, // The actual products array
+      totalPages: Math.ceil(totalProducts / limit),
+      currentPage: Number(page),
+      totalProducts  // Total number of products for frontend to adjust pagination
+    });
   } catch (error) {
     res.status(500).json({ message: 'Error fetching products', error });
   }
 });
+
 
 // Get products by category
 router.get('/products/category/:category', async (req, res) => {
